12f828c96e5ce29dda7d77c977a623150817d56e
Nishant Pawar <nishantpawar1378@gmail.com>
2024-03-30 12:58:53 +0530

    navabar and searchbar

--- book-search-app/src/App.jsx
+++ book-search-app/src/App.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  BrowserRouter, Routes, Route
-} from 'react-router-dom';
-import { AppProvider } from './context.';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './context'; // Import the AppProvider component
 import './index.css';
 import Home from './pages/Home/Home';
 import About from "./pages/About/About";
 import BookList from "./components/BookList/BookList";
 import BookDetails from "./components/BookDetails/BookDetails";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <AppProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/" element = {<Home />}>
-          <Route path = "about" element = {<About />} />
-          <Route path = "book" element = {<BookList />} />
-          <Route path = "/book/:id" element = {<BookDetails />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </AppProvider>
-);
+const App = () => {
+  return (
+    <AppProvider> {/* Wrap the entire application with AppProvider */}
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="book" element={<BookList />} />
+            <Route path="/book/:id" element={<BookDetails />} />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+    </AppProvider>
+  );
+};
+
+export default App; // Exporting the App component as default
+
+ReactDOM.render(<App />, document.getElementById('root'));
